4047a6cf3e9cb81431ef0a38b404c92a50f5ef0d
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-08-19 20:25:57 +0200

    upgraded

--- Scraper.py
+++ Scraper.py
@@ -7,26 +7,30 @@ from Utils.Tokens import Tokens
 
 class Scraper:
 
-    SubRedditsList = open('SubReddits List.txt', 'r')
+    def Scrape():
+        SubRedditsList = open('SubReddits List.txt', 'r')
 
-    MemesSearchAmount: int = int(input('How much memes ya want from each subreddit ? '))
+        MemesSearchAmount: int = int(input('How much memes ya want from each subreddit ? '))
 
-    for line in SubRedditsList:
-            SubReddit = Tokens.Reddit.subreddit(line.strip())
+        for line in SubRedditsList:
+                SubReddit = Tokens.Reddit.subreddit(line.strip())
 
-            print(f'Collecting memes from {line.strip()}!')
+                print(f'Collecting memes from {line.strip()}!')
 
-            for Post in SubReddit.new(limit = MemesSearchAmount):
-                PostUrl: str = Post.url.lower()
+                for Post in SubReddit.new(limit = MemesSearchAmount):
+                    PostUrl: str = Post.url.lower()
 
-                if 'png' in PostUrl or 'jpg' in PostUrl:
+                    if 'png' in PostUrl or 'jpg' in PostUrl:
 
-                    Image = requests.get(PostUrl).content
-                    ImageDoesntExist: bool = not path.exists(f'{MemesPathInfo.ImagePath}{line.strip()}-{Post.id}.png')
-                    
-                    if ImageDoesntExist:
-                        with open(f'{MemesPathInfo.ImagePath}{line.strip()}-{Post.id}.png', 'wb') as ImageDownloader:
-                            ImageDownloader.write(Image)
+                        Image = requests.get(PostUrl).content
+                        ImageDoesntExist: bool = not path.exists(f'{MemesPathInfo.ImagePath}{line.strip()}-{Post.id}.png')
 
-                else:
-                        print('Encounterd a post without a meme *angry face*')
+                        if ImageDoesntExist:
+                            with open(f'{MemesPathInfo.ImagePath}{line.strip()}-{Post.id}.png', 'wb') as ImageDownloader:
+                                ImageDownloader.write(Image)
+
+                    else:
+                            print('Encounterd a post without a meme *angry face*')
+
+
+Scraper.Scrape()
